e3290957a34166e6392e7415a816402ada7d432c
Quentin Guenther <qguenther95@live.com>
2018-04-22 20:43:49 -0700

    set up project

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+
+    // Turn on error reporting
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    // Require autoload
+    require_once('vendor/autoload.php');
+
+    // Create fat-free instance
+    $f3 = Base::instance();
+
+    // Set debug level to dev
+    $f3->set('DEBUG', 3);
+
+    // routes
+
+?>
